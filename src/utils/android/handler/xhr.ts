982b6af45c5e1f46a382f67da86bbcea8cd1b25a
YH <1191759350@qq.com>
2025-06-13 17:56:11 +0800

    commit

--- src/utils/android/handler/xhr.ts
+++ src/utils/android/handler/xhr.ts
@@ -1,12 +1,113 @@
-import { AndroidRequestOptions, Multipart, XMLHttpRequestForAndroid } from "../types/xhr";
+import {
+  AndroidRequestOptions,
+  BodyMultipartField,
+  BodyMultipartBlob,
+  XMLHttpRequestForAndroid,
+  AndroidRequestResult
+} from "../types/xhr";
 import dsBridge from "dsbridge";
 
-export class XHR implements XMLHttpRequestForAndroid {
-  private options: AndroidRequestOptions;
-  private requestId: string;
+class XMLHttpRequestEventTargetForAndroid extends EventTarget implements XMLHttpRequestEventTarget {
+  #onabort: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
+  #onerror: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
+  #onload: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
+  #onloadend: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
+  #onloadstart: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
+  #onprogress: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
+  #ontimeout: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
 
-  onreadystatechange: ((this: XMLHttpRequest, ev: Event) => any) | null;
-  readyState: number;
+  get onabort() {
+    return this.#onabort;
+  }
+
+  set onabort(val: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null) {
+    this.removeEventListener("abort", this.#onabort as any);
+    this.addEventListener("abort", val as any);
+    this.#onabort = val;
+  }
+
+  get onerror() {
+    return this.#onerror;
+  }
+
+  set onerror(val: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null) {
+    this.removeEventListener("error", this.#onerror as any);
+    this.addEventListener("error", val as any);
+    this.#onerror = val;
+  }
+
+  get onload() {
+    return this.#onload;
+  }
+
+  set onload(val: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null) {
+    this.removeEventListener("load", this.#onload as any);
+    this.addEventListener("load", val as any);
+    this.#onload = val;
+  }
+
+  get onloadend() {
+    return this.#onloadend;
+  }
+
+  set onloadend(val: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null) {
+    this.removeEventListener("loadend", this.#onloadend as any);
+    this.addEventListener("loadend", val as any);
+    this.#onloadend = val;
+  }
+
+  get onloadstart() {
+    return this.#onloadstart;
+  }
+
+  set onloadstart(val: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null) {
+    this.removeEventListener("loadstart", this.#onloadstart as any);
+    this.addEventListener("loadstart", val as any);
+    this.#onloadstart = val;
+  }
+
+  get onprogress() {
+    return this.#onprogress;
+  }
+
+  set onprogress(val: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null) {
+    this.removeEventListener("progress", this.#onprogress as any);
+    this.addEventListener("progress", val as any);
+    this.#onprogress = val;
+  }
+
+  get ontimeout() {
+    return this.#ontimeout;
+  }
+
+  set ontimeout(val: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null) {
+    this.removeEventListener("timeout", this.#ontimeout as any);
+    this.addEventListener("timeout", val as any);
+    this.#ontimeout = val;
+  }
+
+  constructor() {
+    super();
+    this.#onabort = null;
+    this.#onerror = null;
+    this.#onload = null;
+    this.#onloadend = null;
+    this.#onloadstart = null;
+    this.#onprogress = null;
+    this.#ontimeout = null;
+  }
+}
+
+class XMLHttpRequestUploadForAndroid extends XMLHttpRequestEventTargetForAndroid implements XMLHttpRequestUpload {}
+
+export class XHR extends XMLHttpRequestEventTargetForAndroid implements XMLHttpRequestForAndroid {
+  private options: AndroidRequestOptions; // android请求选项
+  private requestId: string | undefined; // 请求id（用于取消请求）
+  private responseHeaders: Record<string, string> | undefined; // 响应头
+  private cancelled = false; // 需要被取消
+
+  #onreadystatechange: ((this: XMLHttpRequest, ev: Event) => any) | null;
+  #readyState: number;
   response: any;
   responseText: string;
   responseType: XMLHttpRequestResponseType;
@@ -24,29 +125,37 @@ export class XHR implements XMLHttpRequestForAndroid {
   LOADING: 3;
   DONE: 4;
 
-  onabort: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onerror: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onload: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onloadend: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onloadstart: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onprogress: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  ontimeout: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
+  get onreadystatechange() {
+    return this.#onreadystatechange;
+  }
 
-  composing?: boolean | undefined;
+  set onreadystatechange(val: ((this: XMLHttpRequest, ev: Event) => any) | null) {
+    this.removeEventListener("readystatechange", this.#onreadystatechange as any);
+    this.addEventListener("readystatechange", val as any);
+    this.#onreadystatechange = val;
+  }
+
+  get readyState() {
+    return this.#readyState;
+  }
+
+  set readyState(val) {
+    this.#readyState = val;
+    this.dispatchEvent(new Event("readystatechange"));
+  }
 
   constructor() {
+    super();
+
     this.options = {
       url: "",
       method: "",
       headers: {},
-      timeout: 0,
-      body: "",
-      multiparts: [],
-      cancelable: false
+      timeout: 0
     };
 
-    this.onreadystatechange = null;
-    this.readyState = 0;
+    this.#onreadystatechange = null;
+    this.#readyState = 0;
     this.responseText = "";
     this.responseType = "";
     this.responseURL = "";
@@ -62,180 +171,172 @@ export class XHR implements XMLHttpRequestForAndroid {
     this.HEADERS_RECEIVED = 2;
     this.LOADING = 3;
     this.DONE = 4;
-
-    this.onabort = null;
-    this.onerror = null;
-    this.onload = null;
-    this.onloadend = null;
-    this.onloadstart = null;
-    this.onprogress = null;
-    this.ontimeout = null;
   }
 
   abort(): void {
-    throw new Error("Method not implemented.");
+    if (this.requestId === undefined) {
+      this.cancelled = true;
+      return;
+    }
+    this.handleAbort();
   }
+
   getAllResponseHeaders(): string {
-    throw new Error("Method not implemented.");
+    if (!this.responseHeaders) return "";
+    let headersText = "";
+    for (const key in this.responseHeaders) {
+      headersText += `${key}: ${this.responseHeaders[key]}\r\n`;
+    }
+    return headersText;
   }
+
   getResponseHeader(name: string): string | null {
-    throw new Error("Method not implemented.");
+    if (!this.responseHeaders) return null;
+    return this.responseHeaders[name.toLowerCase()];
   }
+
   open(method: string, url: string | URL): void;
   open(method: string, url: string | URL, async: boolean, username?: string | null, password?: string | null): void;
   open(method: unknown, url: unknown, async?: unknown, username?: unknown, password?: unknown): void {
     this.options.method = (<string>method || "GET").toUpperCase();
     this.options.url = typeof url === "string" ? url : (<URL>url).toString();
+    this.readyState = this.OPENED;
   }
+
   overrideMimeType(mime: string): void {
     throw new Error("Method not implemented.");
   }
-  send(body?: Document | XMLHttpRequestBodyInit | null): void {
-    let bodyContentType: string;
 
-    switch (Object.prototype.toString.call(body)) {
-      case "[object FormData]": {
-        bodyContentType = "multipart/form-data";
-        (body as FormData).forEach((value, key) => {
-          let multipart: Multipart;
-          if (value instanceof File) {
-            multipart = {
-              type: "file",
-              name: key,
-              value: value.webkitRelativePath,
-              filename: value.name,
-              mimeType: value.type
-            };
-          } else {
-            multipart = {
-              type: "field",
-              name: key,
-              value
-            };
-          }
-          this.options.multiparts.push(multipart);
-        });
-        break;
-      }
-      case "[object URLSearchParams]": {
-        bodyContentType = "application/x-www-form-urlencoded";
-        this.options.body = (body as URLSearchParams).toString();
-        break;
-      }
-      default: {
-        bodyContentType = "text/plain";
-        this.options.body = body?.toString() || "";
-      }
-    }
+  async send(body?: Document | XMLHttpRequestBodyInit | null): Promise<void> {
+    const { bodyContentType, bodyText, bodyBlobText, bodyMultiparts } = await handleSendBody(body);
+    this.options.bodyText = bodyText;
+    this.options.bodyBlobText = bodyBlobText;
+    this.options.bodyMultiparts = bodyMultiparts;
     this.options.headers["content-type"] = this.options.headers["content-type"] ?? bodyContentType;
     this.options.headers["accept"] = this.options.headers["accept"] ?? "*/*";
     this.options.timeout = this.timeout;
 
     dsBridge.call("request", this.options, (result: string) => {
-      const { type, data } = JSON.parse(result);
+      if (this.readyState === this.DONE) return; // 请求已完成，不再接收任何响应
+      const { type, data } = <AndroidRequestResult>JSON.parse(result);
       switch (type) {
         case "requestId":
           this.requestId = data;
-          // callCancel();
+          if (this.cancelled) this.handleAbort();
           break;
         case "uploadProgress":
-          // config.onUploadProgress?.(data);
+          this.upload.dispatchEvent(new ProgressEvent("progress", data));
           break;
         case "downloadProgress":
-          // config.onDownloadProgress?.(data);
+          this.dispatchEvent(new ProgressEvent("progress", data));
+          this.readyState = this.LOADING;
           break;
         case "response":
-          // resolve(data);
+          this.status = data.status;
+          this.statusText = data.statusText;
+          this.responseHeaders = data.headers;
+          this.response = data.body;
+          this.responseURL = data.url;
+          this.readyState = this.HEADERS_RECEIVED; // 该状态代表响应头已返回，但实际响应头和响应体是一起回来的，所以和完成状态放在一起
+          this.readyState = this.DONE;
+          this.dispatchEventAll(new ProgressEvent("load"));
+          this.dispatchEventAll(new ProgressEvent("loadend"));
           break;
         case "error":
-          // reject(data);
+          this.readyState = this.DONE;
+          if (data === "timeout") {
+            this.dispatchEventAll(new ProgressEvent("timeout"));
+          } else {
+            this.dispatchEventAll(new ProgressEvent("error"));
+          }
+          this.dispatchEventAll(new ProgressEvent("loadend"));
           break;
       }
     });
+
+    this.dispatchEventAll(new ProgressEvent("loadstart"));
   }
+
   setRequestHeader(name: string, value: string): void {
     const lName = name.toLowerCase();
     const oldValue = this.options.headers[lName];
     this.options.headers[lName] = oldValue !== undefined ? `${oldValue}, ${value}` : value;
   }
-  addEventListener<K extends keyof XMLHttpRequestEventMap>(
-    type: K,
-    listener: (this: XMLHttpRequest, ev: XMLHttpRequestEventMap[K]) => any,
-    options?: boolean | AddEventListenerOptions
-  ): void;
-  addEventListener(
-    type: string,
-    listener: EventListenerOrEventListenerObject,
-    options?: boolean | AddEventListenerOptions
-  ): void;
-  addEventListener(type: unknown, listener: unknown, options?: unknown): void {
-    throw new Error("Method not implemented.");
+
+  /**
+   * @description: 触发所有监听该事件的对象
+   * @param {Event} event 事件
+   */
+  private dispatchEventAll(event: Event) {
+    this.dispatchEvent(event);
+    this.upload.dispatchEvent(event);
   }
-  removeEventListener<K extends keyof XMLHttpRequestEventMap>(
-    type: K,
-    listener: (this: XMLHttpRequest, ev: XMLHttpRequestEventMap[K]) => any,
-    options?: boolean | EventListenerOptions
-  ): void;
-  removeEventListener(
-    type: string,
-    listener: EventListenerOrEventListenerObject,
-    options?: boolean | EventListenerOptions
-  ): void;
-  removeEventListener(type: unknown, listener: unknown, options?: unknown): void {
-    throw new Error("Method not implemented.");
-  }
-  dispatchEvent(event: Event): boolean {
-    throw new Error("Method not implemented.");
+
+  /**
+   * @description: 处理取消请求
+   */
+  private handleAbort() {
+    this.#readyState = this.DONE;
+    dsBridge.call("cancelRequest", this.requestId);
+    this.dispatchEventAll(new ProgressEvent("abort"));
+    this.dispatchEventAll(new ProgressEvent("loadend"));
   }
 }
 
-class XMLHttpRequestUploadForAndroid implements XMLHttpRequestUpload {
-  onabort: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onerror: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onload: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onloadend: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onloadstart: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  onprogress: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  ontimeout: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null;
-  composing?: boolean | undefined;
+/**
+ * @description: 处理发送的body数据
+ * @param {Document | XMLHttpRequestBodyInit | null} body body数据
+ */
+async function handleSendBody(body?: Document | XMLHttpRequestBodyInit | null) {
+  let bodyContentType: string | undefined = undefined;
+  let bodyText: string | undefined = undefined;
+  let bodyBlobText: string | undefined = undefined;
+  let bodyMultiparts: (BodyMultipartField | BodyMultipartBlob)[] | undefined = undefined;
 
-  constructor() {
-    this.onabort = null;
-    this.onerror = null;
-    this.onload = null;
-    this.onloadend = null;
-    this.onloadstart = null;
-    this.onprogress = null;
-    this.ontimeout = null;
+  if (body === null || body === undefined) {
+    return { bodyContentType, bodyText, bodyBlobText, bodyMultiparts };
   }
 
-  addEventListener<K extends keyof XMLHttpRequestEventTargetEventMap>(
-    type: K,
-    listener: (this: XMLHttpRequestUpload, ev: XMLHttpRequestEventTargetEventMap[K]) => any,
-    options?: boolean | AddEventListenerOptions
-  ): void;
-  addEventListener(
-    type: string,
-    listener: EventListenerOrEventListenerObject,
-    options?: boolean | AddEventListenerOptions
-  ): void;
-  addEventListener(type: unknown, listener: unknown, options?: unknown): void {
-    throw new Error("Method not implemented.");
+  if (typeof body === "string") {
+    bodyContentType = "text/plain";
+    bodyText = body;
+  } else if (body instanceof FormData) {
+    bodyContentType = "multipart/form-data";
+    bodyMultiparts = [];
+    for (const [key, value] of body.entries()) {
+      let bodyMultipart: BodyMultipartField | BodyMultipartBlob;
+      if (value instanceof File) {
+        bodyMultipart = {
+          type: "blob",
+          name: key,
+          filePath: value.webkitRelativePath,
+          fileName: value.name,
+          mimeType: value.type,
+          blobText: value.webkitRelativePath ? undefined : await value.text()
+        };
+      } else {
+        bodyMultipart = {
+          type: "field",
+          name: key,
+          value
+        };
+      }
+      bodyMultiparts.push(bodyMultipart);
+    }
+  } else if (body instanceof URLSearchParams) {
+    bodyContentType = "application/x-www-form-urlencoded";
+    bodyText = body.toString();
+  } else if (body instanceof Document) {
+    bodyContentType = "text/html";
+    bodyText = new XMLSerializer().serializeToString(body);
+  } else if (body instanceof Blob) {
+    bodyContentType = body.type;
+    bodyBlobText = await body.text();
+  } else if (body instanceof ArrayBuffer) {
+    bodyBlobText = new TextDecoder().decode(body);
+  } else {
+    bodyBlobText = new TextDecoder().decode(body.buffer);
   }
-  removeEventListener<K extends keyof XMLHttpRequestEventTargetEventMap>(
-    type: K,
-    listener: (this: XMLHttpRequestUpload, ev: XMLHttpRequestEventTargetEventMap[K]) => any,
-    options?: boolean | EventListenerOptions
-  ): void;
-  removeEventListener(
-    type: string,
-    listener: EventListenerOrEventListenerObject,
-    options?: boolean | EventListenerOptions
-  ): void;
-  removeEventListener(type: unknown, listener: unknown, options?: unknown): void {
-    throw new Error("Method not implemented.");
-  }
-  dispatchEvent(event: Event): boolean {
-    throw new Error("Method not implemented.");
-  }
+
+  return { bodyContentType, bodyText, bodyBlobText, bodyMultiparts };
 }
--- src/utils/android/types/xhr.ts
+++ src/utils/android/types/xhr.ts
@@ -5,15 +5,58 @@ export interface AndroidRequestOptions {
   method: string; // 请求方法
   headers: Record<string, string>; // 请求头
   timeout: number; // 超时时间
-  body: string; // body数据
-  multiparts: Multipart[]; // 分块数据集合
-  cancelable: boolean; // 需要取消请求
+  bodyText?: string; // body文本
+  bodyBlobText?: string; // body二进制文本
+  bodyMultiparts?: (BodyMultipartField | BodyMultipartBlob)[]; // body分块数据集合
 }
 
-export interface Multipart {
-  type: "field" | "file"; // 类型
+export interface BodyMultipartField {
+  type: "field"; // 类型
   name: string; // 名称
   value: string; // 值
-  filename?: string; // 文件名称
-  mimeType?: string; // 文件mime
+}
+
+export interface BodyMultipartBlob {
+  type: "blob"; // 类型
+  name: string; // 名称
+  filePath?: string; // 文件路径
+  fileName?: string; // 文件名称
+  mimeType?: string; // mime类型
+  blobText?: string; // 二进制文本
+}
+
+export type AndroidRequestResult =
+  | AndroidRequestResultRequestId
+  | AndroidRequestResultProgress
+  | AndroidRequestResultResponse
+  | AndroidRequestResultError;
+
+export interface AndroidRequestResultRequestId {
+  type: "requestId"; // 类型
+  data: string; // 请求id
+}
+
+export interface AndroidRequestResultProgress {
+  type: "uploadProgress" | "downloadProgress"; // 类型
+  data: {
+    loaded: number; // 当前数据量
+    total: number; // 总数据量
+    lengthComputable: boolean; // 进度是否可以被测量
+  };
+}
+
+export interface AndroidRequestResultResponse {
+  type: "response"; // 类型
+  data: {
+    status: number; // 状态码
+    statusText: string; // 状态文本
+    headers: Record<string, string>; // 响应头集合
+    body: string; // body数据
+    url: string; // 响应url
+  };
+}
+
+export interface AndroidRequestResultError {
+  type: "error"; // 类型
+  data: "timeout" | "other"; // 失败类型
 }
