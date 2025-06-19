82646580ed3f8df6ee88f869f98349b8fe4f47c4
YH <1191759350@qq.com>
2025-06-19 17:54:58 +0800

    commit

--- src/views/test-page/test-page.vue
+++ src/views/test-page/test-page.vue
@@ -8,15 +8,15 @@
 import axios from "axios";
 import BookReader from "./components/book-reader/book-reader.vue";
 import { request } from "@/utils/axios";
-import { crawlers } from "@/utils/crawler";
+// import { crawlers } from "@/utils/crawler";
 
 defineOptions({
   name: "test-page"
 });
 
-const name = "lengku8";
+// const name = "lengku8";
 
-console.log("aa", crawlers[name]);
+// console.log("aa", crawlers[name]);
 
 const CancelToken = axios.CancelToken;
 const source = CancelToken.source();
@@ -87,6 +87,67 @@ request({
 // setTimeout(() => {
 //   source.cancel("3333");
 // }, 2000);
+
+interface AndroidBridgeApiFun<TData = any> {
+  (data: TData, options?: AndroidBridgeApiFunOptions): AndroidBridgeApiFunReturn;
+}
+
+interface AndroidBridgeApiFunOptions {
+  receive: Record<"done" | string, (result: unknown) => void>;
+}
+
+interface AndroidBridgeApiFunReturn {
+  promise: Promise<any>;
+  send: (name: string, data?: any) => void;
+  done: (name: string, data?: any) => void;
+}
+
+interface AndroidBridgeApi {
+  request: (data: AndroidBridgeApiRequestData, options?: AndroidBridgeApiFunOptions) => AndroidBridgeApiFunReturn;
+}
+
+interface AndroidBridgeApiRequestData {
+  url: string;
+  method: string;
+}
+
+interface AndroidBridgeApiRequestReceive {
+  done: (result: string) => void;
+  error: (result: number) => void;
+}
+
+interface TargetObject {
+  keys: (string | symbol)[];
+}
+
+const createTarget = (keys: (string | symbol)[]): TargetObject => {
+  const target = () => {
+    /** empty */
+  };
+  target.keys = keys;
+  return target;
+};
+
+const handler = {
+  get(target: TargetObject, prop: string | symbol): TargetObject {
+    return new Proxy(createTarget([...target.keys, prop]), handler);
+  },
+  apply(target: TargetObject, _thisArg: TargetObject, argArray: any[]) {
+    window["androidBridgeNative"].call({
+      id: Date.now(),
+      callPath: target.keys,
+      data: argArray[0],
+      options: argArray[1]
+    });
+    return 88;
+  }
+};
+
+const bridge: AndroidBridgeApi = new Proxy<any>(createTarget([]), handler);
+
+console.time();
+const res = bridge.request({ url: "33", method: "11" });
+console.timeEnd();
 </script>
 
 <style lang="scss" scoped>
